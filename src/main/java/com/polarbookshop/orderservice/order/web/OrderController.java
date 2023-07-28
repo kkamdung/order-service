f424a321d33470dd99266d0db38a4c8fd8e4e588
FIC08754 <kkamdung@winhub.co.kr>
2023-07-28 12:14:37 +0900

    REST endpoint 추가

---
+++ src/main/java/com/polarbookshop/orderservice/order/web/OrderController.java
@@ -0,0 +1,29 @@
+package com.polarbookshop.orderservice.order.web;
+
+import com.polarbookshop.orderservice.order.domain.Order;
+import com.polarbookshop.orderservice.order.domain.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @GetMapping
+    public Flux<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @PostMapping
+    public Mono<Order> submitOrder(@Valid @RequestBody OrderRequest orderRequest) {
+        return orderService.submitOrder(orderRequest.getIsbn(), orderRequest.getQuantity());
+    }
+
+}
