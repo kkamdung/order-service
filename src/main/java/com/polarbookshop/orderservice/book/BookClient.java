74fc95897a10d30d8b99d4f68a8ecc4636066620
kkamdung <kkamdung@empal.com>
2025-04-01 20:22:37 +0900

    timeout, retryWhen, onErrorResume 적용

--- src/main/java/com/polarbookshop/orderservice/book/BookClient.java
+++ src/main/java/com/polarbookshop/orderservice/book/BookClient.java
@@ -3,7 +3,11 @@ package com.polarbookshop.orderservice.book;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @RequiredArgsConstructor
 @Component
@@ -17,7 +21,11 @@ public class BookClient {
                 .get()
                 .uri(BOOKS_ROOT_API + isbn)
                 .retrieve()
-                .bodyToMono(Book.class);
+                .bodyToMono(Book.class)
+                .timeout(Duration.ofSeconds(3), Mono.empty())
+                .onErrorResume(WebClientResponseException.NotFound.class, exception -> Mono.empty())
+                .retryWhen(Retry.backoff(3, Duration.ofMillis(100)))
+                .onErrorResume(Exception.class, exception -> Mono.empty());
     }
 
 }
