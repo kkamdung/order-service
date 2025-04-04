8bf31c9e6751e0365132215d0e65e91081791ef9
kkamdung <kkamdung@empal.com>
2025-04-04 15:18:19 +0900

    주문 접수 이벤트 생산자 구현, 스트림 바인딩 설정, 래빗 MQ 트랜잭션 지원 설정

--- src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
+++ src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
@@ -2,28 +2,36 @@ package com.polarbookshop.orderservice.order.domain;
 
 import com.polarbookshop.orderservice.book.Book;
 import com.polarbookshop.orderservice.book.BookClient;
+import com.polarbookshop.orderservice.order.event.OrderAcceptedMessage;
 import com.polarbookshop.orderservice.order.event.OrderDispatchedMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final BookClient bookClient;
+    private final StreamBridge streamBridge;
 
     public Flux<Order> getAllOrders() {
         return orderRepository.findAll();
     }
 
+    @Transactional
     public Mono<Order> submitOrder(String isbn, int quantity) {
         return bookClient.getBookByIsbn(isbn)
                 .map(book -> buildAcceptedOrder(book, quantity))
                 .defaultIfEmpty(buildRejectedOrder(isbn, quantity))
-                .flatMap(orderRepository::save);
+                .flatMap(orderRepository::save)
+                .doOnNext(this::publishOrderAcceptedEvent);
     }
 
     private Order buildAcceptedOrder(Book book, int quantity) {
@@ -32,6 +40,18 @@ public class OrderService {
 
     private Order buildRejectedOrder(String bookIsbn, int quantity) {
         return Order.of(bookIsbn, null, null, quantity, OrderStatus.REJECTED);
+    }
+
+    private void publishOrderAcceptedEvent(Order order) {
+        if (!order.status().equals(OrderStatus.ACCEPTED)) {
+            return;
+        }
+
+        OrderAcceptedMessage orderAcceptedMessage = new OrderAcceptedMessage(order.id());
+        log.info("Sending order accepted event with id: {}", order.id());
+
+        boolean result = streamBridge.send("acceptOrder-out-0", orderAcceptedMessage);
+        log.info("Result of sending data for order with id {}: {}", order.id(), result);
     }
 
     public Flux<Order> consumeOrderDispatchedEvent(Flux<OrderDispatchedMessage> flux) {
