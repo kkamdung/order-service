90fb029fa7a090e415797d54dd8351bc3d5eec62
kkamdung <kkamdung@empal.com>
2026-07-03 19:22:10 +0900

    WebClient를 사용해서 도서 정보를 요청하는 웹 클라이언트 작성

---
+++ src/main/java/com/polarbookshop/orderservice/book/BookClient.java
@@ -0,0 +1,23 @@
+package com.polarbookshop.orderservice.book;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@Component
+public class BookClient {
+
+    private static final String BOOKS_ROOT_API = "/books/";
+    private final WebClient webClient;
+
+    public Mono<Book> getBookByIsbn(String isbn) {
+        return webClient
+                .get()
+                .uri(BOOKS_ROOT_API + isbn)
+                .retrieve()
+                .bodyToMono(Book.class);
+    }
+
+}
