1bcba1464a58ea569dcd3e3e8c19e3b4b95981b8
kkamdung <kkamdung@empal.com>
2026-07-05 15:36:20 +0900

    테스트 코드 작성

--- src/test/java/com/polarbookshop/orderservice/OrderServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/orderservice/OrderServiceApplicationTests.java
@@ -1,15 +1,129 @@
 package com.polarbookshop.orderservice;
 
+import com.polarbookshop.orderservice.book.Book;
+import com.polarbookshop.orderservice.book.BookClient;
+import com.polarbookshop.orderservice.order.domain.Order;
+import com.polarbookshop.orderservice.order.domain.OrderStatus;
+import com.polarbookshop.orderservice.order.web.OrderRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+import reactor.core.publisher.Mono;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@AutoConfigureWebTestClient
 class OrderServiceApplicationTests {
 
+    @Container
+    static PostgreSQLContainer postgresql = new PostgreSQLContainer(DockerImageName.parse("postgres:18.4"));
+
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.r2dbc.url", OrderServiceApplicationTests::r2dbcUrl);
+        registry.add("spring.r2dbc.username", postgresql::getUsername);
+        registry.add("spring.r2dbc.password", postgresql::getPassword);
+        registry.add("spring.flyway.url", postgresql::getJdbcUrl);
+    }
+
+    private static String r2dbcUrl() {
+        return String.format("r2dbc:postgresql://%s:%s/%s",
+            postgresql.getHost(),
+            postgresql.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT),
+            postgresql.getDatabaseName());
+    }
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockitoBean
+    private BookClient bookClient;
+
     @Test
-    void contextLoads() {
+    void whenGetOrdersThenReturn() {
+        var bookIsbn = "1234567893";
+        var book = new Book(bookIsbn, "Title", "Author", 9.90);
+
+        given(bookClient.getBookByIsbn(bookIsbn)).willReturn(Mono.just(book));
+
+        var orderRequest = new OrderRequest(bookIsbn, 1);
+        var expectedOrder = webTestClient.post()
+                .uri("/orders")
+                .bodyValue(orderRequest)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(Order.class)
+                .returnResult()
+                .getResponseBody();
+        assertThat(expectedOrder).isNotNull();
+
+        webTestClient.get()
+                .uri("/orders")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBodyList(Order.class)
+                .value(orders -> {
+                    assertThat(orders.stream().filter(order -> order.bookIsbn().equals(bookIsbn)).findAny()).isNotEmpty();
+                });
+    }
+
+    @Test
+    void whenPostRequestAndBookExistsThenOrderAccepted() {
+        var bookIsbn = "1234567899";
+        var book = new Book(bookIsbn, "Title", "Author", 9.90);
+        given(bookClient.getBookByIsbn(bookIsbn)).willReturn(Mono.just(book));
+
+        var orderRequest = new OrderRequest(bookIsbn, 3);
+
+        var createdOrder = webTestClient.post()
+                .uri("/orders")
+                .bodyValue(orderRequest)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(Order.class)
+                .returnResult()
+                .getResponseBody();
+
+        assertThat(createdOrder).isNotNull();
+        assertThat(createdOrder.bookIsbn()).isEqualTo(orderRequest.isbn());
+        assertThat(createdOrder.quantity()).isEqualTo(orderRequest.quantity());
+        assertThat(createdOrder.bookName()).isEqualTo(book.title() + " - " + book.author());
+        assertThat(createdOrder.bookPrice()).isEqualTo(book.price());
+        assertThat(createdOrder.status()).isEqualTo(OrderStatus.ACCEPTED);
+    }
+
+    @Test
+    void whenPostRequestAndBookNotExistsThenOrderRejected() {
+        var bookIsbn = "1234567894";
+        given(bookClient.getBookByIsbn(bookIsbn)).willReturn(Mono.empty());
+
+        var orderRequest = new OrderRequest(bookIsbn, 3);
+
+        var createdOrder = webTestClient.post()
+                .uri("/orders")
+                .bodyValue(orderRequest)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(Order.class)
+                .returnResult()
+                .getResponseBody();
+
+        assertThat(createdOrder).isNotNull();
+        assertThat(createdOrder.bookIsbn()).isEqualTo(orderRequest.isbn());
+        assertThat(createdOrder.quantity()).isEqualTo(orderRequest.quantity());
+        assertThat(createdOrder.status()).isEqualTo(OrderStatus.REJECTED);
     }
 
 }
