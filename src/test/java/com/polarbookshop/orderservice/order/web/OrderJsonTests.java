b4ed374b611d50d31b35616c53345789b99bc295
kkamdung <kkamdung@empal.com>
2026-07-12 14:34:12 +0900

    Order 레코드 수정에 따른 코드 수정

--- src/test/java/com/polarbookshop/orderservice/order/web/OrderJsonTests.java
+++ src/test/java/com/polarbookshop/orderservice/order/web/OrderJsonTests.java
@@ -21,7 +21,7 @@ class OrderJsonTests {
     @Test
     void testSerialize() throws IOException {
         var order = new Order(394L, "1234567890", "Book Name", 9.90, 1, OrderStatus.ACCEPTED,
-                Instant.now(), Instant.now(), 21);
+                Instant.now(), Instant.now(), "jon", "marlena", 21);
         var jsonContent = json.write(order);
 
         assertThat(jsonContent).extractingJsonPathNumberValue("@.id")
@@ -40,6 +40,10 @@ class OrderJsonTests {
                 .isEqualTo(order.createdDate().toString());
         assertThat(jsonContent).extractingJsonPathStringValue("@.lastModifiedDate")
                 .isEqualTo(order.lastModifiedDate().toString());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.createdBy")
+                .isEqualTo(order.createdBy());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.lastModifiedBy")
+                .isEqualTo(order.lastModifiedBy());
         assertThat(jsonContent).extractingJsonPathNumberValue("@.version")
                 .isEqualTo(order.version());
     }
